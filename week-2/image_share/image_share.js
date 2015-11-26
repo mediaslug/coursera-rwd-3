00bbc9dd52284cbe15c760cffe4b6a20dfd869fa
mediaslug <github@mediaslug.com>
2015-11-25 21:17:54 -0600

    added ratings feature

--- week-2/image_share/image_share.js
+++ week-2/image_share/image_share.js
@@ -2,7 +2,7 @@ Images = new Mongo.Collection("images");
 console.log(Images.find().count());
 
 if (Meteor.isClient) {
-  Template.images.helpers({images:Images.find()});
+  Template.images.helpers({images:Images.find({}, {sort:{rating:-1}})});
 
 
   Template.images.events({
@@ -12,17 +12,31 @@ if (Meteor.isClient) {
 
     'click .js-del-image':function(event) {
         var image_id = this._id; // _id refers to a unique identifier for an item in a Mongo collection. "this" refers to the data the template was displaying  
-        $('#'+image_id).hide('slow', function() {
+        $('#'+image_id).hide('hide', function() {
           Images.remove({"_id":image_id}); // "_id":image_id is a mongo filter  
+        } // end images remove function
 
-        });
-    }
 
-  });
-}
+        );
+            console.log("foo");
+
+
+    }, // end click js-del
+
+    'click .js-rate-image':function(event) {
+      var rating = $(event.currentTarget).data("userrating")
+      console.log(rating);
+      var image_id = this.id; // this refers to the data representing the template
+      console.log(image_id);
+      Images.update({_id:image_id}, {$set: {rating:rating}}) // first argument is the id of the image to update. second argument is the thing you want to change
+
+
+    },
+
+
+  }); // end events
+} // end isClient
 
 if (Meteor.isServer) {
   console.log("i am the server ")
 }
-
-console.log("where am I running");
