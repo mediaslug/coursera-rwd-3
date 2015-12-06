8b00048851effadd3d2fafd9ff3d54f72d144071
mediaslug <github@mediaslug.com>
2015-12-05 19:23:35 -0600

    finished off week 3 and starting week 4

---
+++ week-4/image_share/startup.js
@@ -0,0 +1,26 @@
+if (Meteor.isServer) {
+	Meteor.startup(function() {
+		if (Images.find().count() == 0 ) {
+			for (var i=1; i<23; i++) {
+				console.log("img_" + i + ".jpg");
+				Images.insert(
+				{
+     				img_src: "img_" + i + ".jpg",
+      				img_alt: "image number " + i,
+      				createdOn: new Date(),
+   				}	
+
+				); 
+			} // end of for loop to insert images
+
+			// count of images inserted:
+			console.log(Images.find().count());
+
+			
+
+		} // end if imaage
+	}); // end startup
+
+	
+
+} // end if isServer
