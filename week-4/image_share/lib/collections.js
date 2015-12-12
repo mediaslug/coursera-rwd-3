ad4dd765cdd7a4125a6028b07efee0713b7940c4
unknown <pabrams@PABRAMS-72GXQ32.utsystem.local>
2015-12-11 19:54:36 -0600

    finished video on adding security

---
+++ week-4/image_share/lib/collections.js
@@ -0,0 +1,38 @@
+Images = new Mongo.Collection("images");
+
+//set up security on Images collection
+Images.allow({
+	insert:function(userId, doc) {
+		console.log("testing security on image insert");
+		
+		// doc is the image data that we're going to insert
+		// the server has full access to the data the user is attempting to insert
+		// so we can inspect it before we do anything with it
+
+		//console.log(doc);
+
+		if (Meteor.user()) {
+			if (doc.createdBy != userId) { // userId is the id of the person who has attempted the action
+			console.log("denied wrong userId");
+
+				return false;	
+			} else { // the user is logged in and userId matches createdBy
+				console.log("allowed")
+				return true;
+
+			}
+			
+		} else { // user is not logged in
+			console.log("denied not logged in")
+
+			return false;
+		}
+		
+	},
+
+	remove: function() {
+		console.log("testing security on image remove")
+		//console.log(_id);
+		return true;
+	}
+}) 
